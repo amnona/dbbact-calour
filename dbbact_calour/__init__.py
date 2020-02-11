d0730fcf2ce7f12af0e89cc9b7fed23a17974c00
amnona <amnonim@gmail.com>
2020-02-11 13:33:15 +0200

    update version

--- dbbact_calour/__init__.py
+++ dbbact_calour/__init__.py
@@ -1,3 +1,3 @@
 __credits__ = "https://github.com/amnona/dbbact-calour/graphs/contributors"
-__version__ = "2020.2.2"
-__version_numeric__ = 2020.0202
+__version__ = "2020.2.11"
+__version_numeric__ = 2020.0211
