7334324205f5c67270672b31b7db4f90dead2fba
amnona <amnonim@gmail.com>
2020-08-11 23:11:38 +0300

    update version

--- dbbact_calour/__init__.py
+++ dbbact_calour/__init__.py
@@ -3,8 +3,8 @@ from logging.config import fileConfig
 from logging import getLogger, NOTSET, basicConfig
 
 __credits__ = "https://github.com/amnona/dbbact-calour/graphs/contributors"
-__version__ = "2020.7.06"
-__version_numeric__ = 2020.0706
+__version__ = "2020.7.11"
+__version_numeric__ = 2020.0711
 
 # load the logger config
 try:
