b5ecdae0b87456629f3f4619d9a1941361672116
amnona <amnonim@gmail.com>
2023-08-01 16:17:37 +0300

    bump version

--- dbbact_calour/__init__.py
+++ dbbact_calour/__init__.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from logging import getLogger, NOTSET, basicConfig
 
 __credits__ = "https://github.com/amnona/dbbact-calour/graphs/contributors"
-__version__ = '1.16.0'
+__version__ = '1.17.0'
 
 # from .dbbact import DBBact
 
